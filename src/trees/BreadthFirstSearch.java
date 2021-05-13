8c0e9d35f604f1cf46a43670a2505f74336c3fbc
Simran Munjal <simran6882@gmail.com>
2021-05-13 16:33:48 +0530

    New commit

--- src/trees/BreadthFirstSearch.java
+++ src/trees/BreadthFirstSearch.java
@@ -1,5 +1,7 @@
 package trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class BreadthFirstSearch {
@@ -54,10 +56,85 @@ public class BreadthFirstSearch {
 		}
 	}
 	
+	public static void bfsIterative(BinaryTreeNode root) {
+		
+		Queue<BinaryTreeNode> q = new LinkedList();
+		q.add(root);
+		q.add(null);
+		while(!q.isEmpty()) {
+			BinaryTreeNode curr = q.remove();
+			
+			if(curr==null) {
+				System.out.println();
+				if(!q.isEmpty()) {
+					q.add(null);
+				}
+			}
+			else {
+				System.out.print(curr.data+"  ");
+				
+				if(curr.left!=null) {
+					q.add(curr.left);
+				}
+				
+				if(curr.right!=null) {
+					q.add(curr.right);
+				}
+			}
+		}
+	}
+	
+	public static BinaryTreeNode takeInputLevelWise() {
+		
+		Queue<BinaryTreeNode> q = new LinkedList();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter root's data: ");
+		int data = s.nextInt();
+		if(data==-1) {
+			return null;
+		}
+		BinaryTreeNode root = new BinaryTreeNode(data);
+		q.add(root);
+		while(!q.isEmpty()) {
+			BinaryTreeNode curr = q.remove();
+			
+			System.out.println("Enter left child of "+ curr.data+": ");
+			int left = s.nextInt();
+			if(left!=-1) {
+				curr.left = new BinaryTreeNode(left);
+				q.add(curr.left);
+			}
+			System.out.println("Enter right child of "+ curr.data+": ");
+			int right = s.nextInt();
+			if(right!=-1) {
+				curr.right = new BinaryTreeNode(right);
+				q.add(curr.right);
+			}
+		}
+		return root;
+	}
+	
+	public static void mirror(BinaryTreeNode root) {
+		
+		if(root==null)
+			return;
+		
+		mirror(root.left);
+		mirror(root.right);
+		BinaryTreeNode temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+		return;
+	}
+	
 	public static void main(String[] args) {
 		
-		BinaryTreeNode root = takeInput();
-		levelOrder(root);
+		//BinaryTreeNode root = takeInput();
+		//levelOrder(root);
+		BinaryTreeNode root = takeInputLevelWise();
+		bfsIterative(root);
+		mirror(root);
+		bfsIterative(root);
 	}
 
 }
